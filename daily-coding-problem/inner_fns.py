00225f38f0141afb1dc4cd032f2b6fa70b70c591
samk <samkahn101@gmail.com>
2021-03-01 09:15:34 -0500

    inner functions 3/1

---
+++ daily-coding-problem/inner_fns.py
@@ -0,0 +1,38 @@
+"""
+Good morning! Here's your coding interview problem for today.
+This problem was asked by Jane Street.
+
+cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair.
+For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+
+Given this implementation of cons:
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+Implement car and cdr.
+"""
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+
+def car(pair):
+    def inner_f(a, b):
+        return a
+    return pair(inner_f)
+
+
+def cdr(pair):
+    def inner_f(a, b):
+        return b
+    return pair(inner_f)
+
+
+p = cons(3, 4)
+print(car(p))
+print(cdr(p))
