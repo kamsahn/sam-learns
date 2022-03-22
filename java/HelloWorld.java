4d7b6efe5e91dc7a0bf19c145d6c6ae73d9461ee
samk <samkahn101@gmail.com>
2022-03-22 10:24:23 -0400

    playing with java

---
+++ java/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        Animal dog = new Animal();
+        String sound = dog.speak();
+        System.out.println(sound);
+    }
+}
