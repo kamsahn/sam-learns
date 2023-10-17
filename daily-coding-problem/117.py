3a10d09d196ca8a5c99856e242422bb27dfe3aa4
Sam <kahn@Sams-MacBook-Pro.local>
2023-10-17 13:45:48 -0400

    linter changes per fleet

--- daily-coding-problem/117.py
+++ daily-coding-problem/117.py
@@ -7,15 +7,18 @@ level is a "horizontal" slice of the binary tree
 traverse the tree (with recursion) and sum each level
 return the min amount
 """
+
+
 class Node:
     """
     Node class must have a value
     may have left and right references to other nodes
     """
-    def __init__(self, value: int, left = None, right = None):
+    def __init__(self, value: int, left=None, right=None):
         self.value = value
         self.left = left
         self.right = right
+
 
 def get_min_level(tree: Node) -> int:
     """
@@ -24,11 +27,10 @@ def get_min_level(tree: Node) -> int:
     """
     # Create storage
     level_dict = {}
-    level = 0
 
     # Define recursive function
     def get_min_level_helper(level: int, node: Node):
-        if (level in level_dict):
+        if level in level_dict:
             level_dict[level] = level_dict[level] + node.value
         else:
             level_dict[level] = node.value
@@ -40,17 +42,18 @@ def get_min_level(tree: Node) -> int:
         return
 
     # Call recursive function
-    get_min_level_helper(level, tree)
+    get_min_level_helper(0, tree)
 
     # Find lowest sum
-    lowest = 9999 # arbitary high num
+    lowest = 9999  # arbitary high num
     ans_level = None
     for level, total in level_dict.items():
         if total < lowest:
             ans_level = level
-    # ans_level = min(level_dict, key=level_dict.get) # fancy way to do above logic
+    # ans_level = min(level_dict, key=level_dict.get)  # fancy way to do above logic
 
     return ans_level
+
 
 if __name__ == "__main__":
     tree = Node(4, Node(2, Node(1), Node(1)), Node(3, Node(1)))
